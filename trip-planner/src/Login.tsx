1b8899c74938fc8c3af2633e133b5b966627971c
andre-lima-hubia <54004597+fre200tey623@users.noreply.github.com>
2024-07-09 09:12:11 -0700

    App e Login removidos

--- trip-planner/src/Login.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export function Login() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
