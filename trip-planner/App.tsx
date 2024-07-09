6a354f2a2409af1cb2f1e719a4f5fb5157eaa6ab
andre-lima-hubia <54004597+fre200tey623@users.noreply.github.com>
2024-07-08 20:39:14 -0700

    init do projeto e tailwind adicionado ao projeto

---
+++ trip-planner/App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { Login } from "./src/Login";
+
+export default function App() {
+  return (
+    <>
+      <Login />
+      <StatusBar style="auto" />
+    </>
+  );
+}
